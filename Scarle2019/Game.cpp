bb275c0325e770940e56e9c83398ce71eae654a4
Matt Filer <matthew.filer@hotmail.co.uk>
2019-02-27 22:10:39 +0000

    added toby's high quality model

--- Scarle2019/Game.cpp
+++ Scarle2019/Game.cpp
@@ -159,7 +159,7 @@ void Game::Initialize(HWND _window, int _width, int _height)
 	m_3DObjects.push_back(test3);
 
 	//create a "player"
-	Player* test4 = new Player(m_RD, "Standard Kart");
+	Player* test4 = new Player(m_RD, "Test Ramp");
 	m_3DObjects.push_back(test4);
 
 	//point a camera at the player that follows
