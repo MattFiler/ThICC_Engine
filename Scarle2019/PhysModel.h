53be21efebd7efbc881c5e8331ea06860609aeff
Unknown <Toby2.Jones@live.uwe.ac.uk>
2019-03-07 15:41:54 +0000

    Even fixing what he BROKE. Also collision against walls dampens velocity based on reflected angle.

--- Scarle2019/PhysModel.h
+++ Scarle2019/PhysModel.h
@@ -29,9 +29,10 @@ public:
 	void		SetDrag(float _drag) { m_drag = _drag; }
 
 	BoundingOrientedBox getCollider() { return m_collider; };
-	void setCollided(bool _collided) { m_collided = _collided; };
 
-	SDKMeshGO3D* getDebugCollider() { return m_colliderDebug; }
+	SDKMeshGO3D* getDebugCollider() { return m_colliderDebug; };
+
+	void addCurrentCollision(Collision _currentCollision) { m_currentCollisions.push_back(_currentCollision); };
 
 protected:
 
@@ -46,27 +47,26 @@ protected:
 	float m_maxGrav = 60; // The maximum length of m_gravVel. m_gravVel will be clamped to this
 	Vector3 m_acc = Vector3::Zero;
 
-	bool m_hasCollider = false;
+	std::vector<Collision> m_currentCollisions;
 
 	XMFLOAT3 m_collLocalCentre;//Local Centre of the mesh
 	XMFLOAT3 m_collWorldCentre;//World Centre of the mesh and the centre of the bounding box
 	BoundingOrientedBox m_collider; //Bounding box of the model
-	bool m_collided = false; //True if bounding box is inside another
 
 	SDKMeshGO3D* m_colliderDebug = nullptr;
 	bool m_hasCollider = false;
 	PhysModelData m_physData;
 
 	//Corner Pos in Local Space
-	Vector3 m_front_top_left;
-	Vector3 m_front_top_right;
-	Vector3 m_back_bottom_left;
-	Vector3 m_back_bottom_right;
+	Vector3 m_frontTopLeft;
+	Vector3 m_frontTopRight;
+	Vector3 m_backBottomLeft;
+	Vector3 m_backBottomRight;
 	//Corner Pos in World Space
-	Vector3 m_global_front_left;
-	Vector3 m_global_front_right;
-	Vector3 m_global_back_left;
-	Vector3 m_global_back_right;
+	Vector3 m_globalFrontLeft;
+	Vector3 m_globalFrontRight;
+	Vector3 m_globalBackLeft;
+	Vector3 m_globalBackRight;
 	float m_width;
 	float m_length;
 	float m_height;
