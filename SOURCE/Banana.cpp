2909dbb2bdbdc377abde8524888b84f452096d10
Unknown <Evan2.Diamond@live.uwe.ac.uk>
2019-04-21 16:52:53 +0100

    Added normal and spec maps to banana and shell

--- SOURCE/Banana.cpp
+++ SOURCE/Banana.cpp
@@ -4,7 +4,7 @@
 #include "ItemData.h"
 #include <iostream>
 
-Banana::Banana() : Item(Locator::getItemData()->GetItemModelName(BULLET_BILL))
+Banana::Banana() : Item(Locator::getItemData()->GetItemModelName(BANANA))
 {
 	m_mesh->SetDrag(0.9f);
 	m_mesh->SetPhysicsOn(true);
