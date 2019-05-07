e5a7b9565338c4b8e279d93648a80d1b2eb51de5
Unknown <Evan2.Diamond@live.uwe.ac.uk>
2019-05-07 11:03:14 +0100

    Added bomb explosion transparency and bullet bill re-import

--- SOURCE/ItemData.cpp
+++ SOURCE/ItemData.cpp
@@ -38,7 +38,7 @@ ItemData::ItemData() {
 
 /* Get a random item based on the player position */
 ItemType ItemData::GetRandomItem(int _position) {
-	return RED_SHELL;
+	return BOMB;
 	float random_out_of_100 = (rand() % 101);
 
 	for (int i = 0; i < item_count-1; i++) {
