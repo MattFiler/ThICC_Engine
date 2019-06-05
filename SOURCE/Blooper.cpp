073058e6a22ea8e097b123c5db9ee14a8dace04b
Unknown <Evan2.Diamond@live.uwe.ac.uk>
2019-06-05 20:00:00 +0100

    Blooper working except ink

--- SOURCE/Blooper.cpp
+++ SOURCE/Blooper.cpp
@@ -10,19 +10,44 @@ Blooper::Blooper(std::vector<Player*> _players) : m_players(_players)
 
 void Blooper::Tick()
 {
+	for (BlooperData& data : m_blooperDatas)
+	{
+		if (data.m_player->isInvincible() || data.m_player->IsRespawning())
+		{
+			for (int i = 0; i < 3; ++i)
+			{
+				data.m_finished[i] = true;
+				m_usingPlayerData.m_player->GetAnimController()->SwitchModelSet(m_usingPlayerData.m_player->IsGliding() ? "gliding" : "default");
+			}
+		}
+	}
+
 	switch (m_blooperState)
 	{
 		case UP:
 		{
-			std::string gliding = m_player->IsGliding() ? " Gliding" : "";
-			SDKMeshGO3D* model = m_usingPlayerData.m_player->GetAnimController()->GetModelFromSet("Blooper" + gliding, "Blooper")->GetModel();
+			SwitchModelSet(m_usingPlayerData.m_player);
 
-			model->SetPos(Vector3::Lerp(m_usingPlayerData.m_startPos, m_usingPlayerData.m_endPos, m_usingPlayerData.m_lerpPercent));
+			std::string set = m_usingPlayerData.m_player->GetAnimController()->GetCurrentSet();
+			AnimationModel* model = m_usingPlayerData.m_player->GetAnimController()->GetModelFromSet(set, "Blooper");
+
+			model->SetPosOffset(Vector3::Lerp(m_usingPlayerData.m_startPos, m_usingPlayerData.m_endPos, m_usingPlayerData.m_lerpPercent));
 			m_usingPlayerData.m_lerpPercent += m_lerpSpeed * Locator::getGSD()->m_dt;
+
+			DebugText::print("Up: " + std::to_string(model->GetPosOffset().x) + " " + std::to_string(model->GetPosOffset().y) + " " + std::to_string(model->GetPosOffset().z));
 
 			if (m_usingPlayerData.m_lerpPercent >= 1)
 			{
-				m_player->GetAnimController()->SwitchModelSet(m_player->IsGliding() ? "gliding" : "defualt");
+				model->SetPosOffset(m_usingPlayerData.m_startPos);
+				m_usingPlayerData.m_player->GetAnimController()->SwitchModelSet(m_usingPlayerData.m_player->IsGliding() ? "gliding" : "default");
+
+				for (BlooperData& data : m_blooperDatas)
+				{
+					std::string gliding = data.m_player->IsGliding() ? " Gliding" : "";
+					data.m_player->GetAnimController()->SwitchModelSet("Blooper" + gliding);
+					data.m_startPos = data.m_player->GetWorld().Up() * m_height;
+				}
+
 				m_blooperState = DOWN;
 			}
 
@@ -30,12 +55,109 @@ void Blooper::Tick()
 		}
 		case DOWN:
 		{
+			for (BlooperData& data : m_blooperDatas)
+			{
+				if (data.m_finished[0])
+				{
+					continue;
+				}
+
+				SwitchModelSet(data.m_player);
+
+				std::string set = data.m_player->GetAnimController()->GetCurrentSet();
+				AnimationModel* model = data.m_player->GetAnimController()->GetModelFromSet(set, "Blooper");
+				model->SetPosOffset(Vector3::Lerp(data.m_startPos, data.m_endPos, data.m_lerpPercent));
+				DebugText::print("Down " + std::to_string(model->GetPosOffset().x) + " " + std::to_string(model->GetPosOffset().y) + " " + std::to_string(model->GetPosOffset().z));
+
+				data.m_lerpPercent += m_lerpSpeed * Locator::getGSD()->m_dt;
+
+				if (data.m_lerpPercent >= 1)
+				{
+					data.m_endPos = data.m_player->GetAnimController()->GetModelFromSet(set, "Blooper")->GetPosOffset();
+					data.m_startPos = data.m_player->GetWorld().Up() * m_height;
+					data.m_finished[0] = true;
+					++m_stateCount;
+				}
+			}
+
+			if (m_stateCount >= m_blooperDatas.size())
+			{
+				m_stateCount = 0;
+				m_blooperState = INK;
+			}
+
 			break;
 		}
 		case INK:
 		{
+			for (BlooperData& data : m_blooperDatas)
+			{
+				if (data.m_finished[1])
+				{
+					continue;
+				}
+
+				data.m_inkTimeElapsed += Locator::getGSD()->m_dt;
+
+				if (data.m_inkTimeElapsed >= data.m_inkDuration)
+				{
+					data.m_finished[1] = true;
+					++m_stateCount;
+				}
+			}
+
+			if (m_stateCount >= m_blooperDatas.size())
+			{
+				m_stateCount = 0;
+				m_blooperState = LEAVE;
+			}
 			break;
 		}
+		case LEAVE:
+		{
+			for (BlooperData& data : m_blooperDatas)
+			{
+				if (data.m_finished[2])
+				{
+					continue;
+				}
+
+				SwitchModelSet(data.m_player);
+				std::string set = data.m_player->GetAnimController()->GetCurrentSet();
+
+				AnimationModel* model = data.m_player->GetAnimController()->GetModelFromSet(set, "Blooper");
+				model->SetPosOffset(Vector3::Lerp(data.m_startPos, data.m_endPos, data.m_lerpPercent));
+
+				data.m_lerpPercent -= m_lerpSpeed * 2 * Locator::getGSD()->m_dt;
+
+				if (data.m_lerpPercent <= 0)
+				{
+					data.m_player->GetAnimController()->SwitchModelSet(data.m_player->IsGliding() ? "gliding" : "default");
+					model->SetPosOffset(data.m_endPos);
+					++m_finishedCount;
+					data.m_finished[2] = true;
+				}
+			}
+
+			if (m_finishedCount >= m_blooperDatas.size())
+			{
+				FlagForDestoy();
+			}
+			break;
+		}
+	}
+}
+
+void Blooper::SwitchModelSet(Player* _player)
+{
+	std::string set = _player->GetAnimController()->GetCurrentSet();
+	if (_player->IsGliding() && set != "Blooper gliding")
+	{
+		_player->GetAnimController()->SwitchModelSet("Blooper gliding");
+	}
+	else if (set != "Blooper")
+	{
+		_player->GetAnimController()->SwitchModelSet("Blooper");
 	}
 }
 
@@ -44,8 +166,8 @@ void Blooper::Use(Player * player, bool _altUse)
 	setItemInUse(player);
 
 	m_usingPlayerData.m_player = m_player;
-	m_usingPlayerData.m_startPos = m_player->GetAnimController()->GetModelFromSet("Blooper", "Blooper")->GetModel()->GetPos();
-	m_usingPlayerData.m_endPos = Vector3(m_usingPlayerData.m_startPos.x, m_usingPlayerData.m_startPos.y + m_height, m_usingPlayerData.m_startPos.z);
+	m_usingPlayerData.m_startPos = m_player->GetAnimController()->GetModelFromSet("Blooper", "Blooper")->GetPosOffset();
+	m_usingPlayerData.m_endPos = m_player->GetWorld().Up() * m_height;
 	std::string gliding = m_player->IsGliding() ? " Gliding" : "";
 	m_player->GetAnimController()->SwitchModelSet("Blooper" + gliding);
 
@@ -64,8 +186,7 @@ void Blooper::FindPlayers()
 		{
 			BlooperData data = BlooperData();
 			data.m_player = (*it);
-			data.m_endPos = (*it)->GetAnimController()->GetModelFromSet("Blooper", "Blooper")->GetModel()->GetPos();
-			data.m_startPos = Vector3(data.m_endPos.x, data.m_endPos.y + m_height, data.m_endPos.z);
+			data.m_endPos = (*it)->GetAnimController()->GetModelFromSet("Blooper", "Blooper")->GetPosOffset();
 			m_blooperDatas.push_back(data);
 			++it;
 		}
--- SOURCE/Blooper.h
+++ SOURCE/Blooper.h
@@ -10,9 +10,10 @@ public:
 	void HitByPlayer(Player* player) override {};
 	void Use(Player* player, bool _altUse) override;
 
-	void FindPlayers();
 
 private:
+	void FindPlayers();
+	void SwitchModelSet(Player* _player);
 
 	struct BlooperData
 	{
@@ -20,15 +21,21 @@ private:
 		Vector3 m_startPos = Vector3::Zero;
 		Vector3 m_endPos = Vector3::Zero;
 		float m_lerpPercent = 0;
+		float m_inkTimeElapsed = 0;
+		float m_inkDuration = 1;
+		bool m_finished[4] = {false, false, false };
+
 	};
 
+	int m_finishedCount = 0;
+	int m_stateCount = 0;
 	float m_lerpSpeed = 1;
-	enum blooperState { UP, DOWN, INK } m_blooperState = UP;
+	enum blooperState { UP, DOWN, INK, LEAVE } m_blooperState = UP;
 
 	std::vector<BlooperData> m_blooperDatas;
 	BlooperData m_usingPlayerData = BlooperData();
 	std::vector<Player*> m_players;
-	float m_height = 50;
+	float m_height = 10;
 
 };
 
